695d67241404e8a047736e3332b7b3f58ebfc242
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-19 10:30:45 +0000

    fix recaptcha problem
    git-svn-id: https://plugins.svn.wordpress.org/form-maker/trunk@655258 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recaptchalib.php
+++ recaptchalib.php
@@ -44,7 +44,7 @@ define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
  * @param $data - array of string elements to be encoded
  * @return string - encoded request
  */
-  if(!function_exists(_recaptcha_qsencode)){
+  if(!function_exists("_recaptcha_qsencode")){
 function _recaptcha_qsencode ($data) {
         $req = "";
         foreach ( $data as $key => $value )
