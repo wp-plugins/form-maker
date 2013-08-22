96054ced8ad47d5bc8a6db0db71feb7ac13770c1
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-22 13:17:36 +0000

    change demo forms
    git-svn-id: https://plugins.svn.wordpress.org/form-maker/trunk@760664 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recaptchalib.php
+++ recaptchalib.php
@@ -243,5 +243,4 @@ if (!function_exists('_recaptcha_qsencode')) {
     return htmlentities($emailparts[0]) . "<a href='" . htmlentities($url) . "' onclick=\"window.open('" . htmlentities($url) . "', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;\" title=\"Reveal this e-mail address\">...</a>@" . htmlentities($emailparts [1]);
   }
 }
-
 ?>
