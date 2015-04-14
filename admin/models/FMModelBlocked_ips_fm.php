ad321b1474751891979379696f6aed3375146e46
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 08:51:10 +0000

    bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/form-maker/tags/1.7.32@1134319 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/models/FMModelBlocked_ips_fm.php
@@ -0,0 +1,73 @@
+<?php
+
+class FMModelBlocked_ips_fm {
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Events                                                                             //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Constants                                                                          //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Variables                                                                          //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Constructor & Destructor                                                           //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  public function __construct() {
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Public Methods                                                                     //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  public function get_rows_data() {
+    global $wpdb;
+    $where = ((isset($_POST['search_value']) && (esc_html($_POST['search_value']) != '')) ? 'WHERE `ip` LIKE "%' . esc_html($_POST['search_value']) . '%"' : '');
+    $asc_or_desc = ((isset($_POST['asc_or_desc'])) ? esc_html($_POST['asc_or_desc']) : 'desc');
+    $order_by = ' ORDER BY ' . ((isset($_POST['order_by']) && esc_html($_POST['order_by']) != '') ? esc_html($_POST['order_by']) : 'id') . ' ' . $asc_or_desc;
+    if (isset($_POST['page_number']) && $_POST['page_number']) {
+      $limit = ((int) $_POST['page_number'] - 1) * 20;
+    }
+    else {
+      $limit = 0;
+    }
+    $query = "SELECT * FROM " . $wpdb->prefix . "formmaker_blocked " . $where . $order_by . " LIMIT " . $limit . ",20";
+    $rows = $wpdb->get_results($query);
+    return $rows;
+  }
+  
+  public function get_row_data($id) {
+    global $wpdb;
+    if ($id != 0) {
+      $row = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'formmaker_blocked WHERE id="%d"', $id));
+    }
+    else {
+      $row->id = 0;
+      $row->ip = '';
+    }
+    return $row;
+  }
+
+  public function page_nav() {
+    global $wpdb;
+    $where = ((isset($_POST['search_value']) && (esc_html($_POST['search_value']) != '')) ? 'WHERE `ip` LIKE "%' . esc_html($_POST['search_value']) . '%"'  : '');
+    $query = "SELECT COUNT(*) FROM " . $wpdb->prefix . "formmaker_blocked " . $where;
+    $total = $wpdb->get_var($query);
+    $page_nav['total'] = $total;
+    if (isset($_POST['page_number']) && $_POST['page_number']) {
+      $limit = ((int) $_POST['page_number'] - 1) * 20;
+    }
+    else {
+      $limit = 0;
+    }
+    $page_nav['limit'] = (int) ($limit / 20 + 1);
+    return $page_nav;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Getters & Setters                                                                  //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Private Methods                                                                    //
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // Listeners                                                                          //
+  ////////////////////////////////////////////////////////////////////////////////////////
+}
